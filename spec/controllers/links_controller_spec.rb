75e536cc345a95b4f588942c073d14a86ca6702d
Zachary Nagler <zachary.nagler@gmail.com>
2014-10-31 17:40:50 -0400

    added user controller test

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe LinksController, :type => :controller do
 
+
+    # describe "GET index" do
+
+    # it "get index" do
+    #   get :sessions
+    #   expect(response).to be_success
+    #   expect(response).to have_http_status(200)
+    #   # expect(assigns(:sum)).to eq(22)
+    # end
+
+  end
+
 end
