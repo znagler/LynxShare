c21896944f60131fe01d0b8617a045545a654ca3
Francisco De La Cruz <helloquico@gmail.com>
2014-10-31 10:20:45 -0400

    dummy rspec tests

--- spec/models/user_link_spec.rb
+++ spec/models/user_link_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe UserLinks, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe UserLink, :type => :model do
+  it "One + One = Two" do
+    expect(1+1).to equal(2)
+  end
+
+  it "One + One != Three" do
+    expect(1+1).to_not equal(3)
+  end
 end
