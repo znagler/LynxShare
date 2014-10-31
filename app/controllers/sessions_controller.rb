25fdd0a6bd97c6737832796af6c87b8026f7873b
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-10-31 12:07:32 -0400

    add create method in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+  
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+      
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to current_user
+    else
+      # 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
 end
