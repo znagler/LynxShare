25749c299cfccd60061d54eb82894d9546b4eec3
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-10-30 23:41:25 -0400

    create associations in relationship model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,4 @@
 class Relationship < ActiveRecord::Base
-  belongs_to :follower
-  belongs_to :followed_user
+  belongs_to :follower, class_name: :User, foreign_key: :follower_id
+  belongs_to :followed_user, class_name: :User, foreign_key: :followed_user_id
 end
