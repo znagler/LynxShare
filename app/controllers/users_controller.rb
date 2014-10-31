9102d87dc824e8eaa8709494d9d06eae45981ac9
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-10-31 14:26:48 -0400

    add create method in users controller with registration logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,21 @@ class UsersController < ApplicationController
   def index
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in(@user)
+      flash[:success] = "Welcome to the sample app!"
+      redirect_to current_user
+    else
+      render 'new'  
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation)
+    end
+
 end
