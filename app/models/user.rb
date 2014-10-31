72bc084c31172027bf2317a14e2ad327dec6e78f
Zachary Nagler <zachary.nagler@gmail.com>
2014-10-31 15:54:04 -0400

    fix tests after adding bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,10 @@ class User < ActiveRecord::Base
   has_many :followers, through: :follower_relationships, source: :follower
   has_many :followed_users, through: :followed_relationships, source: :followed_user
 
-  validates :password_digest, :presence => true
-  validates :password_digest, length: { minimum: 5 }
+  validates :password, length: { minimum: 5 }
+  validates :username, :presence => true
+  validates :username, length: { minimum: 5 }
+
 
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,17 +12,17 @@ RSpec.describe User, :type => :model do
     end
     it "accounts for username length" do
       FactoryGirl.build(:user, :username => "123").should_not be_valid
-      FactoryGirl.build(:user, :username => "real_username").should be_valid
+      FactoryGirl.build(:user).should be_valid
     end
   end
 
   context "Validates passwords" do
     it "should not allow empty passwords" do
-      FactoryGirl.build(:user, :password_digest => "").should_not be_valid
+      FactoryGirl.build(:user, :password => "").should_not be_valid
     end
     it "accounts for password length" do
-      FactoryGirl.build(:user, :password_digest => "123").should_not be_valid
-      FactoryGirl.build(:user, :password_digest => "real_password").should be_valid
+      FactoryGirl.build(:user, :password => "123").should_not be_valid
+      FactoryGirl.build(:user, :password => "real_password", :password_confirmation => "real_password").should be_valid
     end
 
   end
