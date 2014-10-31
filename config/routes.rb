0c1944858da5799decd787901802e3e4735c9607
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-10-31 14:22:40 -0400

    create signup route that maps to users/new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 LynxShare::Application.routes.draw do
+  resources :users
+
+  get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   root 'sessions#new'
