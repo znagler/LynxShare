ee2f79dfbd7f203577c11019827cf93b423eb403
Zachary Nagler <zachary.nagler@gmail.com>
2014-10-31 15:42:34 -0400

    delete temp and log files

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  include SessionsHelper
+  
+  def new
+  end
+  
+  def create
+    user = User.find_by(username: params[:session][:username].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to current_user
+    else
+      # 'Invalid username/password combination'
+      render 'new'
+    end
+  end
+
+end
