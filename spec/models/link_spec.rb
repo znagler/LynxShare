606babbdb553b48e90260f1a93f613f16d5bd90e
Zachary Nagler <zachary.nagler@gmail.com>
2014-10-31 12:19:55 -0400

    finished Link Model tests

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -2,15 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Link, :type => :model do
 
-  context "Associates with users" do
-  it { should have_many(:user_links)}
-  it { should have_many(:users)}
-  end
+    context "Associates with users" do
+    it { should have_many(:user_links)}
+    it { should have_many(:users)}
+    end
 
-  context "Associates with tags" do
-  it { should have_many(:link_tags)}
-  it { should have_many(:tags)}
-  end
+    context "Associates with tags" do
+    it { should have_many(:link_tags)}
+    it { should have_many(:tags)}
+    end
 
+    context "Validates URLs" do
+    it "should not allow empty URL" do
+      FactoryGirl.build(:link, :url => "").should_not be_valid
+    end
+    it "should not allow invalid" do
+      FactoryGirl.build(:link, :url => "google  com").should_not be_valid
+      FactoryGirl.build(:link, :url => "googlecom").should_not be_valid
+    end
+
+    it "should allow valid urls" do
+      FactoryGirl.build(:link, :url => "google.com").should be_valid
+    end
+
+  end
 
 end
