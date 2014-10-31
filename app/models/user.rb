8b230140598229b46ac7fd27eae1fa81fb0189f4
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-10-30 23:44:09 -0400

    create followers relationship in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :follower_relationships, class_name: :Relationship, foreign_key: :followed_user_id
+  has_many :followed_relationships, class_name: :Relationship, foreign_key: :follower_id
+  has_many :followers, through: :follower_relationships, source: :follower
+  has_many :followed_users, through: :followed_relationships, source: :followed_user
 end
