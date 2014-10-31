017eec90b150455d94ace18bf96fd3d33806ad02
Francisco De La Cruz <helloquico@gmail.com>
2014-10-31 16:12:05 -0400

    Seed application

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
+gem "faker", "~> 1.4.3"
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
@@ -40,7 +42,6 @@ group :development, :test do
 end
 
 group :test do
-  gem "faker", "~> 1.4.3"
   gem "capybara", "~> 2.4.3"
   gem "database_cleaner", "~> 1.3.0"
   gem "launchy", "~> 2.4.2"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+path = Rails.root.join('db/seed_data.yaml')
+seed_data = YAML.load File.read path
+
+# delete all old records
+
+seed_data.keys.each do |table_name|
+  ActiveRecord::Base.connection.execute "truncate #{table_name}"
+end
+
+# insert records
+
+def insert_sql table_name, attrs
+  table = Arel::Table.new(table_name)
+  insert_manager = Arel::InsertManager.new(ActiveRecord::Base)
+  insert_manager.insert(attrs.map { |key, val| [ table[key], val ] })
+  insert_manager.to_sql
+end
+
+sql = seed_data.map do |table_name, records|
+  records.map do |attrs|
+    insert_sql table_name, attrs
+  end
+end
+
+sql.flatten.each do |insert_sql|
+  ActiveRecord::Base.connection.execute insert_sql
+end
