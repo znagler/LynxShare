1cee69ee4c752433f4bc1ebbab0076192f3000bf
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-10-31 14:32:34 -0400

    registration link added to login page.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,6 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(username: params[:session][:username].downcase)
-      
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to current_user
