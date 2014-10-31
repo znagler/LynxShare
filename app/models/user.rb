13290bab472fcae3d6baa3a1e71e335c9885978d
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-10-30 23:46:18 -0400

    create relationship between user model and user_link model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :user_links
+  # has_many :links, through: :user_links
+
   has_many :follower_relationships, class_name: :Relationship, foreign_key: :followed_user_id
   has_many :followed_relationships, class_name: :Relationship, foreign_key: :follower_id
   has_many :followers, through: :follower_relationships, source: :follower
