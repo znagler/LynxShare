98c5c6591d263f63a7ac3c056c889ba4acb0c435
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-11-01 17:30:19 -0400

    add dependent destroy for link_tag - when user deletes a link all the LinkTags associated with the link will also be deleted

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,7 @@
 class Link < ActiveRecord::Base
   has_many :user_links
   has_many :users, through: :user_links
-  has_many :link_tags
+  has_many :link_tags, dependent: :destroy
   has_many :tags, through: :link_tags
   validates :url, :presence => true
   validates :url, format: { with: /\A\S+\.\S+\Z/i }
