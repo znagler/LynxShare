b0b6d88783271dcab28d660e0f1ee91b92c5f771
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-11-01 17:17:40 -0400

    create helpers to format tags when the user enters the tag

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -3,4 +3,17 @@ module LinksHelper
   def add_hashtag(tag)
     "#" + tag
   end
+
+  def pad(string)
+    " " + string
+  end
+
+  def remove_hashtags(words)
+    words.map!{ |word| word.gsub("#","") }
+  end
+
+  def format_tags(words_array)
+    remove_hashtags(pad(words_array).scan(/[#\s](\S*)/).flatten)
+  end
+  
 end
