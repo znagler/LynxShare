59b62127b0b1a5932e5ac5cd05c24e3c846c927d
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-11-01 17:19:11 -0400

    add create_tags method in Link model that creates tags from an array of tags passed in

--- app/models/link.rb
+++ app/models/link.rb
@@ -5,4 +5,12 @@ class Link < ActiveRecord::Base
   has_many :tags, through: :link_tags
   validates :url, :presence => true
   validates :url, format: { with: /\A\S+\.\S+\Z/i }
+
+
+  def create_tags(tags)
+    tags.each do |tag|
+      LinkTag.create(link: self, tag: Tag.find_or_create_by(name: tag))
+    end
+  end
+
 end
