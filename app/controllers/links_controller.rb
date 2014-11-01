efb63a3a11e4e3dfe17454b0c2e437b5273ccbe7
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-11-01 16:54:11 -0400

    middle of something

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -7,5 +7,17 @@ class LinksController < ApplicationController
   def new
     @link = Link.new
   end
-  
+
+  def create
+    binding.pry
+    current_user.links.create(link_params)
+    redirect_to '/links'
+  end
+
+  private
+
+  def link_params
+      params.require(:link).permit(:url)
+  end
+
 end
--- app/models/user_link.rb
+++ app/models/user_link.rb
@@ -2,4 +2,5 @@ class UserLink < ActiveRecord::Base
   belongs_to :user
   belongs_to :link
   validates :user_id,:link_id, :presence => true
+  # validates_uniqueness_of :link_id, :scope => :tag_id
 end
