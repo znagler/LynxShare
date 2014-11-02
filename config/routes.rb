e8bb538f46922885db81785b26cb338e11d14089
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-11-02 01:38:17 -0400

    add resources :relationships

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ LynxShare::Application.routes.draw do
   resources :users
   resources :links
   resources :tags
-
+  resources :relationships
+  
   post '/search' => 'links#ajax'
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
