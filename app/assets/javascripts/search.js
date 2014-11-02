afae1d16dc941431ca1276a29e510efd9cf33853
Zachary Nagler <zachary.nagler@gmail.com>
2014-11-02 00:25:21 -0400

    finished search

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,29 +1,23 @@
 console.log("reading file")
 $( document ).ready(function() {
   console.log("doc ready")
+  setEventListeners()
+});
 
-$( "[data-search='searchBar']" ).keyup(function() {
-  $('.link,.tag').show()
+function setEventListeners(){
+  $( "[data-search='searchBar']" ).keyup(function() {
   var sendData = {currentString:( $(this).val())};
-  if ($(this).val() != "") $('.link,.tag').hide()
+  $('.link,.tag').remove()
   console.log("before ajax")
   $.ajax({
     url: '/search',
     type: 'post',
-    dataType: 'json',
-    data: sendData
+    data: sendData,
+    dataType: "html"
   })
   .done(function(response) {
-    console.log("success");
-    console.log(response);
-  for (i = 1; i <= 10; i++) {
-    console.log(response["item"+i]);
-  }
+    $('#linkzone').html(response)
+    })
   })
-
-
-});
-
-
-});
+}
 
