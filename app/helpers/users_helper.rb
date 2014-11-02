046c907bcd8e7f179b231c12af78f248d8540c1f
Zachary Nagler <zachary.nagler@gmail.com>
2014-11-02 12:49:09 -0500

    basic user search

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,20 @@
 module UsersHelper
+    def fetch_users(capacity,user_id,str = "")
+    loaded = false
+    users = []
+    while !loaded do
+      User.all.each do |user|
+        # binding.pry
+        users.push(user) if user.username.include?(str) && user.id != user_id
+        if users.size == capacity
+          loaded = true
+          break
+        end
+      end
+      loaded = true
+    end
+    # binding.pry
+    users.uniq
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ LynxShare::Application.routes.draw do
   resources :links
   resources :tags
   resources :relationships
-  
+
+  post '/user_search' => 'users#search'
   post '/link_search' => 'links#search'
   post '/tag_search' => 'tags#search'
   get 'signup' => 'users#new'
