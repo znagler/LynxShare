57d803ddfc70bb7cc34943dbccd6825d43745991
Zachary Nagler <zachary.nagler@gmail.com>
2014-11-02 18:42:16 -0500

    basic tag pages

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,6 +5,12 @@ class TagsController < ApplicationController
     # @tags = current_user.tags
   end
 
+  def show
+    @tag = Tag.find(params[:id])
+    @links = (current_user.links & @tag.links)
+  end
+
+
   def search
     tag_array = fetch_tags(10,current_user.id,params["currentString"])
     render(:partial => "tag_container", :locals => {:tags => tag_array})
