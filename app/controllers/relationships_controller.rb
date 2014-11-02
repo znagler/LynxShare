76a09b29cc7081b9172978428a6a2c3845173bd8
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-11-02 17:39:39 -0500

    ability to unfollow people works

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,16 +1,17 @@
 class RelationshipsController < ApplicationController
 
   def index
-    @users = current_user.followed_users.uniq
+    @users = current_user.followed_users
     @relationship = Relationship.new
   end
 
   def create
     Relationship.create(followed_user_id: params[:relationship][:user_id], follower: current_user)
-    redirect_to '/relationships'
+    redirect_to '/users'
   end
 
-  def delete
-    binding.pry
+  def destroy
+    Relationship.find(params[:id]).destroy
+    redirect_to '/relationships'
   end
 end
